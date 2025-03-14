b19df3c8d661f2f131b6395fe695267ea0ca9b42
Lovelymercedanet <ruslantooyoung@gmail.com>
2025-03-14 21:21:12 +0500

    routes and models

---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const User = require("./models/User.js");
+
+// Регистрация пользователя
+router.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: "Username and password are required" });
+    }
+
+    try {
+        const newUser = new User({ username, password });
+        /* */
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+        /* */
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error registering user" });
+    }
+});
+
+// Логин пользователя
+// router.post("/login", async (req, res) => {
+//     const { username, password } = req.body;
+
+//     try {
+//         const user = await User.findOne({ username });
+//         if (!user || user.password !== password) {
+//             return res.status(401).json({ error: "Invalid credentials" });
+//         }
+//         res.status(200).json({ message: "Login successful" });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: "Error logging in" });
+//     }
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const cors = require("cors");
 const PORT = process.env.PORT ?? 3000;
 const app = express();
+require("dotenv").config();
+
+app.use(
+    cors({
+        origin: "http://localhost:3000", // Разрешить запросы только с этого домена
+        methods: ["GET", "POST"], // Разрешенные HTTP-методы
+        allowedHeaders: ["Content-Type", "Authorization"], // Разрешенные заголовки
+    })
+);
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to mongodb"))
     .catch((e) => console.error(`Error connecting: ${e}`))
 mongoose.Promise = global.Promise;
-const db = mongoose.connection;
 
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.listen(PORT, () => {
